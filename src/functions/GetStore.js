568f9a01a622a9de30a31443625371f4e10acb18
Brxnson <79931117+brxnson@users.noreply.github.com>
2021-09-05 23:04:31 +0100

    .

--- src/functions/GetStore.js
+++ src/functions/GetStore.js
@@ -4,6 +4,6 @@ const fetch = require("node-fetch")
 module.exports = async function(ApiToken) {
     return fetch(`https://${defaultRoute}${apiEndpoints.getStore}`,
     {method: "GET", body: JSON.stringify(), headers: { "Content-Type": "application/json", "TRN-Api-Key": ApiToken }})
-    .then(async data => { return await data.json() })
+    .then(async data => {return await data.json()})
     .catch(err => {throw new Error(err)})
 }
