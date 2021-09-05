58582ddf2b287e4fc43d5d3271138a23a79e89d7
Brxnson <79931117+brxnson@users.noreply.github.com>
2021-09-05 23:19:11 +0100

    update code

--- Examples/GetPlayerStats.js
+++ Examples/GetPlayerStats.js
@@ -1,4 +1,4 @@
 const FortniteTracker = require ("fn-tracker-api")
-const client = new FortniteTracker("Your fortnite tracker api token")
+const client = new FortniteTracker("Your fortnite-tracker api token")
 
 client.getPlayerStats("Brxnson", "kbm").then(data => console.log(data))
--- Examples/GetStore.js
+++ Examples/GetStore.js
@@ -1,4 +1,4 @@
 const FortniteTracker = require ("fn-tracker-api")
-const client = new FortniteTracker("Your fortnite tracker api token")
+const client = new FortniteTracker("Your fortnite-tracker api token")
 
 client.getStore().then(data => console.log(data))
